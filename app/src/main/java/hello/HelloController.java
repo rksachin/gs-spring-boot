b9593e7d0ca04f2a72275aa5110bcbe54c65ee96
rksachin <rk.sachin@gmail.com>
2021-04-14 12:00:02 +0000

    changed

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Spring Boot!4";
+        return "Hello from Spring Boot!5";
     }   
 }
